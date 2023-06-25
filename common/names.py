aa35adf60d6ec6c24326e37eceaf270a2807e0f7
Moein <122750743+yolowex@users.noreply.github.com>
2023-06-25 09:53:56 +0330

    Added common, core and gui modules

---
+++ common/names.py
@@ -0,0 +1,16 @@
+# standard
+import random
+import sys
+import os
+import time
+
+from typing import Optional,List,Dict,Union
+
+# third party
+import pygame as pg
+from pygame.locals import *
+from pygame import Color,Font,FRect,Vector2,Surface
+
+from pygame._sdl2 import Renderer, Texture, Window # noqa
+
+
---
+++ common/resources.py
@@ -0,0 +1,8 @@
+from common.names import *
+from core.event_holder import EventHolder
+
+screen: Optional[Surface] = None
+event_holder: Optional[EventHolder] = None
+
+window: Optional[Window] = None
+renderer: Optional[Renderer] = None
