ff21100ff63798d900673698de3eeeaa63dc3eeb
Moein <122750743+yolowex@users.noreply.github.com>
2023-06-25 13:14:44 +0330

    Fixed mac platform constants problem

--- common/constants.py
+++ common/constants.py
@@ -13,13 +13,13 @@ LINUX_DISTRIBUTION = ""
 LINUX_KERNEL_VERSION = ""
 MACOS_VERSION = ""
 MACOS_BUILD_NUMBER = ""
-
+MACOS_ARCHITECTURE = ""
 
 def export_platform_constants() :
     # inline globalization does not work prior to python 3.6 version
     global IS_WINDOWS,WINDOWS_BUILD_NUMBER,WINDOWS_SERVICE_PACK,WINDOWS_RELEASE_VERSION
     global IS_LINUX,LINUX_DISTRIBUTION,LINUX_KERNEL_VERSION
-    global IS_MAC,MACOS_VERSION,MACOS_BUILD_NUMBER
+    global IS_MAC,MACOS_VERSION,MACOS_BUILD_NUMBER,MACOS_ARCHITECTURE
 
 
     # Determine the platform
@@ -32,9 +32,10 @@ def export_platform_constants() :
 
     # Print the platform
     print("Current platform: ", current_platform)
-    version_info = platform.win32_ver()
+
 
     if IS_WINDOWS :
+        version_info = platform.win32_ver()
         WINDOWS_RELEASE_VERSION = version_info[0]
         WINDOWS_SERVICE_PACK = version_info[1]
         WINDOWS_BUILD_NUMBER = version_info[2]
@@ -54,20 +55,24 @@ def export_platform_constants() :
         print("Kernel Version:", LINUX_KERNEL_VERSION)
 
     elif IS_MAC :
-        MACOS_VERSION = version_info[0]
-        MACOS_BUILD_NUMBER = version_info[1]
+        mac_version = platform.mac_ver()
+
+        MACOS_VERSION = mac_version[0]
+        MACOS_BUILD_NUMBER = mac_version[1]
+        MACOS_ARCHITECTURE = mac_version[2]
 
         print("macOS Version:", MACOS_VERSION)
         print("Build Number:", MACOS_BUILD_NUMBER)
+        print("Architecture:", MACOS_ARCHITECTURE)
+
     else :
         print("Warning: running on an unknown platform")
 
 
-
+"""
+    I added this try/except block just in case, it might be unnecessary thou
+"""
 try:
-    """
-    I think this is unnecessary, but I added this try/except block just in case
-    """
     export_platform_constants()
 except Exception as e:
     # Print the traceback and error message
