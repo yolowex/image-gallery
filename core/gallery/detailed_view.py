ddd254246db6fc338bc3fd4749d0290b0a5d8965
Moein <122750743+yolowex@users.noreply.github.com>
2023-06-29 12:28:38 +0330

    Added boxes for different gui sections in DetialedView

--- core/gallery/detailed_view.py
+++ core/gallery/detailed_view.py
@@ -9,7 +9,51 @@ from helper_kit.relative_rect import RelRect
 
 class DetailedView:
     def __init__(self):
-        self.current_image_rect = RelRect(cr.ws,0.2,0.1,0.78,0.7)
+        image_pos = Vector2(0.2,0.1)
+        image_size = Vector2(0.8,0.65)
+
+        self.top_box = RelRect(cr.ws,0,0,1,0.05)
+        self.bottom_box = RelRect(cr.ws,0,0.95,1,0.05)
+
+        self.detail_box = RelRect(cr.ws,image_pos.x,image_pos.y-0.05,image_size.x,0.05)
+        self.image_box = RelRect(cr.ws,image_pos,image_size)
+
+        self.left_box = RelRect(
+            cr.ws,
+            0,
+            image_pos.y,
+            image_pos.x,
+            image_size.y
+        )
+
+        self.info_box = RelRect(
+            cr.ws,
+            0,
+            self.top_box.rect.y+self.top_box.rect.h,
+            image_pos.x,
+            abs(self.top_box.rect.y+self.top_box.rect.h-image_pos.y)
+        )
+
+        self.log_box = RelRect(
+            cr.ws,
+            0,
+            self.left_box.rect.y+self.left_box.rect.h,
+            image_pos.x,
+            abs(self.left_box.rect.y+self.left_box.rect.h-self.bottom_box.rect.y)
+        )
+
+        self.preview_box = RelRect(
+            cr.ws,
+            image_pos.x,
+            image_pos.y+image_size.y,
+            image_size.x,
+            abs(image_pos.y+image_size.y-self.bottom_box.rect.y)
+        )
+
+
+
+
+
 
     def check_events(self):
         ...
@@ -18,9 +62,19 @@ class DetailedView:
         ...
 
     def render(self):
-        cr.renderer.draw_color = colors.FOREST_GREEN
-        cr.renderer.draw_rect(self.current_image_rect.get())
+        cr.renderer.draw_color = colors.CRIMSON
+        cr.renderer.draw_rect(self.top_box.get())
+        cr.renderer.draw_rect(self.bottom_box.get())
 
+        cr.renderer.draw_color = colors.FOREST_GREEN
+        cr.renderer.draw_rect(self.log_box.get())
+        cr.renderer.draw_rect(self.left_box.get())
+        cr.renderer.draw_rect(self.info_box.get())
+
+        cr.renderer.draw_color = colors.NAVY
+        cr.renderer.draw_rect(self.image_box.get())
+        cr.renderer.draw_rect(self.detail_box.get())
+        cr.renderer.draw_rect(self.preview_box.get())
 
         if cr.event_holder.should_render_debug:
             self.render_debug()
@@ -31,3 +85,4 @@ class DetailedView:
 
 
 
+# todo: find the purpose of DetailedView.log_box box & choose a better name
