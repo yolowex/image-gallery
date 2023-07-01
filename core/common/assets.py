e0d19be1880d77048ed1137b2b986b21d3dbda66
Moein <122750743+yolowex@users.noreply.github.com>
2023-07-01 03:59:17 +0330

    Added test_assets to source dirs

--- core/common/assets.py
+++ core/common/assets.py
@@ -7,4 +7,4 @@ pics: list[Texture] = []
 def init_assets() :
     global pics
     pics = [Texture.from_surface(cr.renderer, pg.image.load(os.path.abspath("./test_assets/" + i))) for i in
-        os.listdir("./test_assets")]
+        os.listdir(os.path.abspath("./test_assets"))]
