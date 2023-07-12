326b3e25081a54f00182facea4c937009720bec7
Moein <122750743+yolowex@users.noreply.github.com>
2023-07-13 02:54:01 +0330

    Updated audio_extract_test

--- tests/audio_extract_test.py
+++ tests/audio_extract_test.py
@@ -1,12 +1,15 @@
 import subprocess
 import os
 
-in_video_path =  os.path.abspath("../test_assets/movie.mkv")
+in_video_path =  os.path.abspath("../test_assets/clip.mp4")
 
 bin_path = os.path.abspath("../ffmpeg/bin/ffmpeg.exe")
 out_audio_path =  os.path.abspath("../dump/audio3.mp3")
 
 command = f"{bin_path} -i {in_video_path} -vn -acodec libmp3lame -qscale:a 2 {out_audio_path}"
 
-output = subprocess.run(command,shell=True,capture_output=True)
-print(output)
+try:
+    subprocess.check_call(command, shell=True)
+    print("Command executed successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Command failed with return code {e.returncode}.")
