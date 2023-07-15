20dae181680a2fbce1041c1f9eb3158f1b148d0f
Moein <122750743+yolowex@users.noreply.github.com>
2023-07-15 23:05:38 +0330

    Added indirect app entry support

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ import os
 
 import os
 import pathlib
+import sys
 
 print(f"current working directory is {os.getcwd()}\n")
+print(sys.argv)
 
-
-file_path = pathlib.Path(__file__).resolve().as_posix()
+file_path = pathlib.Path(sys.argv[0]).resolve().as_posix()
 file_name = file_path.split("/")[-1]
-file_path = file_path[:len(file_path)-len(file_name)-1]
+file_path = file_path[: len(file_path) - len(file_name) - 1]
 print(f"entry file is located at: {file_path}\n")
 print(f"changing the cwd to {file_path}\n")
-os.chdir(file_path)
+if file_path != "":
+    os.chdir(file_path)
 
 print(f"current working directory is {os.getcwd()}\n")
 
-input()
-
-1/0
 
 current_platform = platform.system()
 app_name = "Foto Folio"
@@ -40,7 +39,7 @@ if not os.path.exists(app_data_path):
     os.mkdir(app_data_path)
 
 log = Log(log_path)
-
+error = False
 try:
     log.write_log("Starting the app...", LogLevel.INFO)
     from core.entry import Entry
@@ -51,6 +50,9 @@ try:
 except Exception as e:
     error_log = "Unexpected error: " + str(e) + "\n" + traceback.format_exc()
     log.write_log(error_log, LogLevel.FATAL)
+    error = True
 
 
 log.write_log("Closing the app...", LogLevel.INFO)
+if error:
+    input("Press any key to exit")
--- tests/path_test.py
+++ tests/path_test.py
@@ -1,12 +1,13 @@
 import os
 import pathlib
+import sys
 
 print(f"current working directory is {os.getcwd()}\n")
+print(sys.argv)
 
-
-file_path = pathlib.Path(__file__).resolve().as_posix()
+file_path = pathlib.Path(sys.argv[0]).resolve().as_posix()
 file_name = file_path.split("/")[-1]
-file_path = file_path[:len(file_path)-len(file_name)-1]
+file_path = file_path[: len(file_path) - len(file_name) - 1]
 print(f"entry file is located at: {file_path}\n")
 print(f"changing the cwd to {file_path}\n")
 os.chdir(file_path)
