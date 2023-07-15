67f07450e35ff68a84514df15fbf78e086038e7e
Moein <122750743+yolowex@users.noreply.github.com>
2023-07-15 22:37:23 +0330

    Added path test

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ import platform
 import traceback
 import os
 
+import os
+import pathlib
+
+print(f"current working directory is {os.getcwd()}\n")
+
+
+file_path = pathlib.Path(__file__).resolve().as_posix()
+file_name = file_path.split("/")[-1]
+file_path = file_path[:len(file_path)-len(file_name)-1]
+print(f"entry file is located at: {file_path}\n")
+print(f"changing the cwd to {file_path}\n")
+os.chdir(file_path)
+
+print(f"current working directory is {os.getcwd()}\n")
+
+input()
+
+1/0
+
 current_platform = platform.system()
 app_name = "Foto Folio"
 app_data_path = "."
