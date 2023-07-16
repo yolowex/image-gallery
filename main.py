cb806b03c48e64f679e5758c3c7a4f24e41ce599
Moein <122750743+yolowex@users.noreply.github.com>
2023-07-16 09:37:55 +0330

    stdout clean up

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import os
 import pathlib
 import sys
 
-print(f"current working directory is {os.getcwd()}\n")
+print(f"current working directory is {os.getcwd()}")
 print(sys.argv)
 
 file_path = pathlib.Path(sys.argv[0]).resolve().as_posix()
 file_name = file_path.split("/")[-1]
 file_path = file_path[: len(file_path) - len(file_name) - 1]
-print(f"entry file is located at: {file_path}\n")
-print(f"changing the cwd to {file_path}\n")
+print(f"entry file is located at: {file_path}")
+print(f"changing the cwd to {file_path}")
 if file_path != "":
     os.chdir(file_path)
 
