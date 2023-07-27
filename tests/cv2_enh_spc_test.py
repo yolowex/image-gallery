18b82e50da662e1c61073d2e694be3bb4604b156
Moein <122750743+yolowex@users.noreply.github.com>
2023-07-27 10:14:33 +0330

    Added sharpness and saturation to enhancement test

--- tests/cv2_enh_spc_test.py
+++ tests/cv2_enh_spc_test.py
@@ -39,7 +39,9 @@ def modify_brightness(input_image_path, output_image_path, brightness_factor):
     except Exception as e:
         print(f"Error: {e}")
 
+
 from PIL import Image, ImageEnhance
+
 
 def modify_contrast(input_image_path, output_image_path, contrast_factor):
     try:
@@ -48,7 +50,6 @@ def modify_contrast(input_image_path, output_image_path, contrast_factor):
 
         # Create an enhancer object for contrast
         enhancer = ImageEnhance.Contrast(image)
-
 
         # Modify the contrast using the factor provided
         modified_image = enhancer.enhance(contrast_factor)
@@ -60,8 +61,11 @@ def modify_contrast(input_image_path, output_image_path, contrast_factor):
     except Exception as e:
         print(f"Error: {e}")
 
+
 # untested
-def modify_color_balance(input_image_path, output_image_path, red_factor, green_factor, blue_factor):
+def modify_color_balance(
+    input_image_path, output_image_path, red_factor, green_factor, blue_factor
+):
     try:
         # Open the image
         image = Image.open(input_image_path)
@@ -75,12 +79,59 @@ def modify_color_balance(input_image_path, output_image_path, red_factor, green_
         b = b.point(lambda i: i * blue_factor)
 
         # Merge the modified channels back into a new image
-        modified_image = Image.merge('RGB', (r, g, b))
+        modified_image = Image.merge("RGB", (r, g, b))
 
         # Save the modified image to the output path
         modified_image.save(output_image_path)
 
         print("Color balance modification successful!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def modify_saturation(input_image_path, output_image_path, saturation_factor):
+    try:
+        # Open the image
+        image = Image.open(input_image_path)
+
+        # Convert the image to HSL color model
+        hsl_image = image.convert("HSV")
+
+        # Split the HSL image into individual channels
+        h, s, l = hsl_image.split()
+
+        # Modify the intensity of the saturation channel using the provided factor
+        s = s.point(lambda i: i * saturation_factor)
+
+        # Merge the modified channels back into a new HSL image
+        modified_hsl_image = Image.merge("HSV", (h, s, l))
+
+        # Convert the modified HSL image back to RGB color model
+        modified_image = modified_hsl_image.convert("RGB")
+
+        # Save the modified image to the output path
+        modified_image.save(output_image_path)
+
+        print("Saturation modification successful!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def modify_sharpness(input_image_path, output_image_path, sharpness_factor):
+    try:
+        # Open the image
+        image = Image.open(input_image_path)
+
+        # Create an enhancer object for sharpness
+        enhancer = ImageEnhance.Sharpness(image)
+
+        # Modify the sharpness using the factor provided
+        modified_image = enhancer.enhance(sharpness_factor)
+
+        # Save the modified image to the output path
+        modified_image.save(output_image_path)
+
+        print("Sharpness modification successful!")
     except Exception as e:
         print(f"Error: {e}")
 
@@ -92,7 +143,7 @@ wing = 10
 x = 0
 
 for i in range(total):
-    x += 1 / total * 2.5
+    x += 1 / total * 10
     out_path = f"{out_dir}/{i}_{str(x)[:5]}.jpeg"
 
-    modify_contrast(input_path, out_path, x)
+    modify_sharpness(input_path, out_path, x)
