dc1d8fee340527db69698fbea73c456c0630e572
Moein <122750743+yolowex@users.noreply.github.com>
2023-07-31 09:18:20 +0330

    Improved InfoView's rendering

--- core/gallery/info_view.py
+++ core/gallery/info_view.py
@@ -5,6 +5,7 @@ from core.common.constants import colors as colors
 from core.common.names import *
 import core.common.resources as cr
 from core.gallery.content import Content
+from gui.text_view import TextView
 from helper_kit.relative_rect import RelRect
 from core.common import utils, assets
 
@@ -50,8 +51,6 @@ class InfoView:
 
             return fun
 
-        step_w = 1 / 3
-
         self.button_1_text = Texture.from_surface(
             cr.renderer, self.font.render("Folders", True, cr.color_theme.text_0)
         )
@@ -61,6 +60,39 @@ class InfoView:
         self.button_3_text = Texture.from_surface(
             cr.renderer, self.font.render("Edit", True, cr.color_theme.text_0)
         )
+
+        step_w = 1 / 3
+
+        self.box_1 = RelRect(
+            fun,
+            step_w * 0,
+            0,
+            step_w,
+            1,
+            use_param=True,
+        )
+
+        self.box_2 = RelRect(
+            fun,
+            step_w * 1,
+            0,
+            step_w,
+            1,
+            use_param=True,
+        )
+
+        self.box_3 = RelRect(
+            fun,
+            step_w * 2,
+            0,
+            step_w,
+            1,
+            use_param=True,
+        )
+
+        self.text_view_1 = TextView(self.box_1, False, "Folders", y_scale=0.5)
+        self.text_view_2 = TextView(self.box_2, False, "Tags", y_scale=0.5)
+        self.text_view_3 = TextView(self.box_3, False, "Edit", y_scale=0.5)
 
         self.button_1_box = RelRect(
             make_fun(self.button_1_text.get_rect().size),
@@ -96,15 +128,9 @@ class InfoView:
         self.selected_box_index = SelectedInfoView.FOLDERS
 
     def update_texts(self):
-        self.button_1_text = Texture.from_surface(
-            cr.renderer, self.font.render("Folders", True, cr.color_theme.text_0)
-        )
-        self.button_2_text = Texture.from_surface(
-            cr.renderer, self.font.render("Tags", True, cr.color_theme.text_0)
-        )
-        self.button_3_text = Texture.from_surface(
-            cr.renderer, self.font.render("Edit", True, cr.color_theme.text_0)
-        )
+        self.text_view_1.update()
+        self.text_view_2.update()
+        self.text_view_3.update()
 
     @property
     def selected_box(self) -> RelRect:
@@ -146,44 +172,13 @@ class InfoView:
         self.check_click()
 
     def render(self):
-        self.selected_box.render(
-            cr.color_theme.color_1,
-            cr.color_theme.button,
-            cr.color_theme.color_0,
-            rect=self.fun(self.selected_box.rect),
-        )
+        self.text_view_1.render()
+        self.text_view_2.render()
+        self.text_view_3.render()
 
-        s1 = self.button_1_box.get()
-        s2 = self.button_2_box.get()
-        s3 = self.button_3_box.get()
-
-        r1 = self.fun(self.button_1_box.rect)
-        s1.center = r1.center
-        r2 = self.fun(self.button_2_box.rect)
-        s2.center = r2.center
-        r3 = self.fun(self.button_3_box.rect)
-        s3.center = r3.center
-
-        cut_1 = utils.cut_rect_in(r1, s1)
-        cut_2 = utils.cut_rect_in(r2, s2)
-        cut_3 = utils.cut_rect_in(r3, s3)
-
-        src_rect_1 = utils.mult_rect(
-            cut_1[1], self.button_1_text.width, self.button_1_text.height
-        )
-        src_rect_2 = utils.mult_rect(
-            cut_2[1], self.button_2_text.width, self.button_2_text.height
-        )
-        src_rect_3 = utils.mult_rect(
-            cut_3[1], self.button_3_text.width, self.button_3_text.height
-        )
-
-        # src_rect_1.center = self.button_1_box.get().center
-        # cut_1[0].center = self.button_1_box.get().center
-
-        self.button_1_text.draw(src_rect_1, cut_1[0])
-        self.button_2_text.draw(src_rect_2, cut_2[0])
-        self.button_3_text.draw(src_rect_3, cut_3[0])
+        rect = self.fun(self.selected_box.rect)
+        cr.renderer.draw_color = cr.color_theme.button
+        cr.renderer.draw_rect(rect)
 
 
 # todo: find a better name for this class
